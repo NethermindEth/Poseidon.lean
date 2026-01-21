3bc55e3608ac8ddec67366e3c376195896e7539d
Daniel Britten <Coda-Coda@users.noreply.github.com>
2026-01-21 19:34:18 +1300

    Update dependencies to v4.27.0-rc1

--- lakefile.lean
+++ lakefile.lean
@@ -8,12 +8,11 @@ lean_lib Poseidon where
   precompileModules := true
 
 require YatimaStdLib from git
-  "https://github.com/argumentcomputer/YatimaStdLib.lean" @ "v4.12.0"
+  "https://github.com/NethermindEth/YatimaStdLib.lean" @ "v4.27.0-rc1"
 
 require LSpec from git
-  "https://github.com/argumentcomputer/LSpec" @ "v4.12.0"
+  "https://github.com/NethermindEth/LSpec" @ "v4.27.0-rc1"
 
 lean_exe Tests.RoundNumbers
 lean_exe Tests.RoundConstants
 lean_exe Tests.Hash
-
