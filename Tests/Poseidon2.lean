6a6663ac035a8eda32df7ff01134338d6a7206c5
Daniel Britten <Coda-Coda@users.noreply.github.com>
2026-01-29 19:59:32 +1300

    Revert "Remove non-property-based-tests"
    This reverts commit e18964426b9ef1aaeb04166b423b929d0f2c1e70.

--- Tests/Poseidon2.lean
+++ Tests/Poseidon2.lean
@@ -67,6 +67,14 @@ instance List.sampleableExt [SampleableExt α] : SampleableExt (List α) where
 def input₁ : Array Nat := Array.range 24
 
 def tests : TestSeq := group "Poseidon2 Lean vs Rust tests" $
+  test "input₁" ((
+    (FFI.poseidon2Permute24Nat input₁).toList.map (fun x ↦ Int.ofNat x))
+    = 
+    ((Poseidon2.hashInputWithCtx BabyBear24.hashProfile BabyBear24.lurkContext input₁).toList.map (fun x ↦ x.rep))) $
+  test "input₁-reverse" ((
+    (FFI.poseidon2Permute24Nat input₁).toList.map (fun x ↦ Int.ofNat x))
+    = 
+    ((Poseidon2.hashInputWithCtx BabyBear24.hashProfile BabyBear24.lurkContext input₁).toList.map (fun x ↦ x.rep))) $
   check "PBT" (∀ (i : List Nat), ((
     (FFI.poseidon2Permute24Nat i.toArray).toList.map (fun x ↦ Int.ofNat x))
     = 
