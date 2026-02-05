9bf196e6a6c0ae8c83ff6410cfcc83e1bc3e35df
Daniel Britten <Coda-Coda@users.noreply.github.com>
2026-02-05 14:30:28 +1300

    Add example using width 16

--- Tests/Poseidon2.lean
+++ Tests/Poseidon2.lean
@@ -4,7 +4,8 @@ import Poseidon.Parameters.BabyBear
 open Poseidon2
 open BabyBear (p)
 
+def input16 : Array <| Zmod p := Array.iota 15 |>.map .mk
 def input24 : Array <| Zmod p := Array.iota 23 |>.map .mk
 
+#eval Poseidon2.hashInputWithCtx BabyBear16.hashProfile BabyBear16.lurkContext input16
 #eval Poseidon2.hashInputWithCtx BabyBear24.hashProfile BabyBear24.lurkContext input24
-
